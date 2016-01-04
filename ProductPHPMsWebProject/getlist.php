06e3d98b87e66ec65a362cb1d4c6765647790738
reflash <reflash-ne@yandex.ru>
2016-01-04 23:12:22 +0300

    Connected to database, added code for sort order and lazy loading

--- ProductPHPMsWebProject/getlist.php
+++ ProductPHPMsWebProject/getlist.php
@@ -1,19 +1,5 @@
 <?php
-$product1 = array(
-	'id' => 2,
-	'name' => 'SomeProduct2',
-	'description' => 'This is a simple product',
-	'price' => '1242.0',
-	'image_url' => 'http://kevy.com/wp-content/uploads/2013/09/total-product-marketing.jpg');
+include "db_facade.php";
 
-$product2 = array(
-	'id' => 0, 
-	'name' => 'SomeProduct', 
-	'description' => 'This is a simple product', 
-	'price' => '1112.0',
-	'image_url' => 'http://kevy.com/wp-content/uploads/2013/09/total-product-marketing.jpg');
-
-$products = array($product1,$product2);
-
-echo json_encode($products);
+echo getList(0,50,true);
 ?>
